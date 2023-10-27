aa4c090949003ea14ec0f055cb2f1cd6780b812c
Miguel Sánchez <a21miguelsv@iessanclemente.net>
2023-10-27 12:50:57 +0200

    ChatApp

--- UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
+++ UD04_1_ChatApp/app/src/main/java/com/example/ud04_1_chatapp/MainActivity.kt
@@ -4,22 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        toolbar.setupWithNavController(navHostFragment.navController)
         setSupportActionBar(toolbar)
+
+        //Host de navegacion
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //Configuracion de la barra de navegacion
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builderApp.build()
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+
+        //Panel lateral
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        builderApp.setOpenableLayout(drawerLayout)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
